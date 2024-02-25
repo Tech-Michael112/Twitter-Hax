021d8b24ff87227c5ce69c61be894d3a85d822f9
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-02-25 21:56:34 +0100

    Update Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -1,1 +1,10 @@
-import cap
+import os, sys, platform,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    #mport i
+    os.system('clear')
+    os.system('git pull')
+    import cap
+   # os.system('python F-CLONE.so')
+else:
+    exit()
